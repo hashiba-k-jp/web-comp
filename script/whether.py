f15772112b3a8a11121c48ec076d85e767737401
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-07-19 14:13:58 +0900

    whether

---
+++ script/whether.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from datetime import datetime
+from datetime import timedelta
+import unicodedata
+
+
+URL = "https://www.data.jma.go.jp/developer/xml/feed/extra_l.xml"
+TIMESPAN = 30
+
+# This should NOT be fixed ... but not enough time.
+TARGET_REGION = "仙台管区気象台"
+
+def search(region, check_time):
+    # get info from JMA server
+    response = requests.get(URL)
+    response.encoding = response.apparent_encoding
+    # print(response.text)
+    soup = bs(response.content, 'xml')
+
+    # get detail data
+    entries = soup.find_all('entry')
+    targets_region = [e for e in entries if e.find('name').string==region]
+    targets = [t for t in targets_region if t.find('title').string=="府県気象情報"]
+    # print(targets)
+
+    for target in targets:
+        update_time = target.find('updated').string
+        # print(update_time)
+        issued_time = datetime(
+            year=int(update_time[0:4]),month=int(update_time[5:7]),day=int(update_time[8:10]),
+            hour=int(update_time[11:13]),minute=int(update_time[14:16]),second=int(update_time[17:19])
+        ) + timedelta(hours=9)
+        print(issued_time, issued_time > check_time)
+
+        if issued_time > check_time:
+            # send message here !
+            content = target.find('content').string
+            content = content.replace('】', '】\n')
+            content = unicodedata.normalize('NFKC', content)
+            text =  f"{issued_time.year:04}年{issued_time.month:02}月{issued_time.day:02}日"+\
+                    f"{issued_time.hour:02}時{issued_time.minute:02}分\n"+\
+                    f"{content}"+\
+                    f"\n{TARGET_REGION}発表"
+            print(text)
+        else:
+            pass
+
+
+def main():
+    # This program is estimeted to be run in each 30 minutes.
+    dt_now = datetime.now()
+    dt_now = datetime(2023, 7, 19, 5, 30, 0) # This is a sample datetime
+
+    check_time = dt_now - timedelta(
+        minutes = (dt_now.minute % TIMESPAN) + TIMESPAN,
+        seconds = dt_now.second,
+        microseconds = dt_now.microsecond
+    )
+    # If TIMESPAN = 30:
+        # for exsample, 14:30 will be 14:00 ; send data issued after 14:00
+        # for exsample, 06:01 will be 05:30 ; send data issued after 05:00
+    print(check_time)
+
+    search(
+        region = TARGET_REGION,
+        check_time = check_time
+    )
+
+if __name__=="__main__":
+    main()
