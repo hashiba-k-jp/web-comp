7766a026e1feb383e8037edd5fff51b4e3665ac6
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-08-04 14:13:37 +0900

    $

--- script/send.py
+++ script/send.py
@@ -12,7 +12,7 @@ def send_msg(text):
     test_send_command = f"curl -v -X POST https://api.line.me/v2/bot/message/broadcast \
     -H \'Content-Type: application/json\' \
     -H \'Authorization: Bearer {{ { LINEAPI_TOKEN } }}\' \
-    -d \'{{ \
+    -d $\'{{ \
         \"messages\":[ \
             {{ \
                 \"type\":\"text\", \
